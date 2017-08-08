7cdebc3baeba6d7e6eca4866901e44fa34671714
RobertZK <technoguyrob@gmail.com>
2017-08-08 16:25:22 -0500

    initial commit.

---
+++ R/package.vault.R
@@ -0,0 +1,6 @@
+#' Simple accessors for Hashicorp's vault in R
+#'
+#' @name vault
+#' @import stringr
+#' @docType package
+NULL
---
+++ tests/test-all.R
@@ -0,0 +1,3 @@
+library("testthat")
+library("vault")
+test_check("vault")
