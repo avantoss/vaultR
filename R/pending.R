470fd2ff6643d258cee46454e3c2d69f4490a152
robertzk <technoguyrob@gmail.com>
2017-08-08 16:36:00 -0500

    Simple example

--- R/pending.R
+++ R/pending.R
@@ -1,4 +1,10 @@
-pending <- function() {
-  TRUE
+#' @export
+pending <- function(a) {
+  a + 1
+}
+
+#' @export
+vault.read <- function(key, mode = "text") {
+  system2("vault", c("read", key))
 }
 
